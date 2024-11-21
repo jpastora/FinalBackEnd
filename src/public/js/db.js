f102af5e8b993232f6bf596dc5e889198e20e044
Joseph Pastora <joseph.pastora@gmail.com>
2024-11-20 19:51:06 -0600

    Eliminación del archivo de conexión a la base de datos

--- src/public/js/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const DB_URI = 'mongodb://localhost:27017/registro';
-
-mongoose.connect(DB_URI, {})
-    .then(() => console.log('Conexión exitosa a la base de datos'))
-    .catch(err => console.error(err));
